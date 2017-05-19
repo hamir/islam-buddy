1b9096e7bdbeca746b44042ef991161d8eb46b2a
Hashim Mir <hamir@hamir-macbookpro.roam.corp.google.com>
2017-05-19 01:58:49 -0700

    gitignore, deployment test

--- server/api.py
+++ server/api.py
@@ -1,6 +1,10 @@
-from flask import Flask, request
-from salah.daily_prayer import DailyPrayer
 import json
+
+from flask import Flask
+from flask import request
+from flask import make_response
+from salah.daily_prayer import DailyPrayer
+
 
 
 app = Flask(__name__)
@@ -12,10 +16,22 @@ def hello_world():
   return 'Welcome to the Islam Buddy API!'
 
 
-@app.route('/salah')
+@app.route('/salah', methods=['POST', 'GET'])
 def get_salah():
-  if not request.args.has_key('lat') or not request.args.has_key('lng'):
+  params = request.get_json(silent=True, force=True)
+
+  print '============================'
+  print 'params = ', params
+  location = params.get('location')
+  print 'location = ', location
+  print 'lat = ', params.get('location').get('latitude')
+  print 'lng = ', params.get('location').get('longitude')
+
+  if not params.has_key('lat') or not request.args.has_key('lng'):
     return 'Please provide a lat and lng.'
+
   prayer_times = \
       daily_prayer.GetPrayerTimes(request.args.get('lat'), request.args.get('lng'))
-  return json.dumps(prayer_times, indent=4)
+  r = make_response(json.dumps(prayer_times, indent=4))
+  r.headers['Content-Type'] = 'application/json'
+  return r

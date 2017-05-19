b6d4d1244cdba37518976edb5aab3e347416746f
Hashim Mir <hamir@hamir-macbookpro.roam.corp.google.com>
2017-05-19 04:04:06 -0700

    gets rid of server/

--- server/salah/salah_com_fetcher_test.py
+++
@@ -1,8 +0,0 @@
-import salah_com_fetcher
-import requests
-
-def test_fetcher():
-  """Tests that the fetcher gets data from salah.com"""
-  print salah_com_fetcher.GetDailyPrayerTimes(37.3541079, -121.9552355)
-
-test_fetcher()

238311efe80f0be6086ef051318955e0569e8311
Hashim Mir <hamir@hamir-macbookpro.roam.corp.google.com>
2017-05-19 00:22:19 -0700

    Supports passing in lat/lng GET parameters.

--- server/api.py
+++ server/api.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from salah.daily_prayer import DailyPrayer
 import json
 
@@ -14,6 +14,8 @@ def hello_world():
 
 @app.route('/salah')
 def get_salah():
+  if not request.args.has_key('lat') or not request.args.has_key('lng'):
+    return 'Please provide a lat and lng.'
   prayer_times = \
-      daily_prayer.GetPrayerTimes(37.3541079, -121.9552355)
+      daily_prayer.GetPrayerTimes(request.args.get('lat'), request.args.get('lng'))
   return json.dumps(prayer_times, indent=4)

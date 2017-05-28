a3fb74a25cea38193fc0805e704f30c95a88972b
ahamdy <a27med@gmail.com>
2017-05-28 07:26:32 +0000

    Add Iqama scraper functionality

--- gmaps_API.py
+++ gmaps_API.py
@@ -1,14 +1,18 @@
 import requests
 import json
 
-
 _GMAPS_API_GEOCODE_URL = 'https://maps.googleapis.com/maps/api/geocode/json'
+
+# Use for Staging GAE
 _GMAPS_API_GEOCODE_KEY = 'AIzaSyBC9cKscPGfI0Ge0uPJxO29ru0qLvxfcdA'
 
-def GetGeocode(city, state, country):
-  """Gets the longitude and latitude from the Google Maps Geocode API.
+# Use for Production GAE
+#_GMAPS_API_GEOCODE_KEY = 'AIzaSyBTm8Tq2_27EHG9sylGxpcwZk2B4ynjiJU'
 
-  Performs a POST request on the Google Maps Geocode API
+def GetGeocode(city, state, country):
+  """Gets the longitude and latitude from the Google Maps API.
+
+  Performs a POST request on the Google Maps API
   to get the longitude and latitude from the provided city/state/country
 
   Args:
@@ -18,7 +22,7 @@ def GetGeocode(city, state, country):
 
   Returns: a dict containing the longitude and latitude
   """
-  
+
   address_params = ''
   if city:
     address_params += city + " "
@@ -26,7 +30,7 @@ def GetGeocode(city, state, country):
     address_params += state + " "
   if country:
     address_params += country + " "
-  
+
   # set up the parameters in the format expected by the Google Maps Geocode API
   post_params = {
     'address': address_params,
@@ -36,9 +40,37 @@ def GetGeocode(city, state, country):
   request = requests.post(_GMAPS_API_GEOCODE_URL, params=post_params)
   print 'here = ', request.text
   response = json.loads(request.text).get("results")[0].get("geometry").get("location")
-  print 'response from GMAPS Geocode API', response
+  print 'response from GMAPS Geocode', response
   return response
 
-def LatLngToCity(lat, lng):
-  # TODO(hamdy): make appropriate call to gmaps
-  return 'santa clara'
+def ReverseGeocodeCity(lat, lng):
+  """Returns city from the Google Maps API based on Lat and Lng.
+
+  Performs a POST request on the Google Maps API
+  to get the city from the provided latitude and longitude
+
+  Args:
+    lat: a double representing the latitude
+    lng: a double representing the longitude
+
+  Returns: a string containing the city
+  """
+
+  address_params = ('%.16f' % lat) + ',' + ('%.16f' % lng)
+
+  # set up the parameters in the format expected by the Google Maps Geocode API
+  post_params = {
+    'latlng': address_params,
+    'key': _GMAPS_API_GEOCODE_KEY,
+  }
+  print 'GMAPS Reverse Geocode post_params = ', post_params
+  request = requests.post(_GMAPS_API_GEOCODE_URL, params=post_params)
+  print 'GMAPS Reverse Geocode response = ', request.text
+  response = json.loads(request.text).get("results")[0].get("address_components")
+  
+  for address in response:
+    if address.get("types")[0] == "locality":
+      print 'response from GMAPS Reverse Geocode', address
+      return address.get("long_name")
+  # if no address returned, return None
+  return None

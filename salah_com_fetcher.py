8210b7db82ec31e703a4712c910d1d478026e288
ahamdy <a27med@gmail.com>
2017-05-29 02:30:41 +0000

    Adding in vs at speech handler

--- salah_com_fetcher.py
+++ salah_com_fetcher.py
@@ -9,7 +9,7 @@ _SALAH_API_URL = 'http://www.salah.com/times/get'
 The 'salah.com' API provides a response in the following format:
 "Prayers": {
   "2017": { "5": { 17": {
-    # actual prayer times yere
+    # actual prayer times are
     "Fajr": "5 AM"
     ...
   }}}
@@ -38,7 +38,7 @@ def GetDailyPrayerTimes(lat, lng):
     'lg': lng,
   }
   print 'post_data = ', post_data
-  request = requests.post(_SALAH_API_URL, data=post_data)
+  request = requests.post(_SALAH_API_URL, data=post_data, timeout=15)
   if request.status_code == 500:
     return {}
   print 'here = ', request.text
@@ -47,4 +47,48 @@ def GetDailyPrayerTimes(lat, lng):
   # dig into the response until we find the prayer times
   for i in range(_PRAYER_TIMES_RESPONSE_DEPTH):
     response = response.itervalues().next()
+
   return response
+
+# For follow-on queries, we get conversation_type "ACTIVE" while original queries are type "NEW".
+def BuildJSON(prayer_name, raw_query, resolved_query, intent, masjid_name="", geo_city="", geo_state_us="", geo_country="", conversation_type="NEW", permission="false"):
+  data = {}
+  data['lang'] = 'en'
+  data['status'] = {'error_type': 'success', 'code': 200}
+  parameters = {}
+  parameters['PrayerName'] = prayer_name
+  parameters['MasjidName'] = masjid_name  
+  parameters['geo-city'] = geo_city
+  parameters['geo-state-us'] = geo_state_us
+  parameters['geo-country'] = geo_country
+
+  contexts = {}
+  # TODO: Make the PrayerName field capitalized/canonicalized if needed.
+  context_parameters = {'PrayerName.original': prayer_name, 'PrayerName': prayer_name, 'geo-city': [geo_city]}
+
+  intent_params = {}
+  if (intent == 'WHEN_IS_START_TIME_INTENT'):
+    intent_params['name'] = 'google_assistant_welcome'
+    intent_params['parameters'] = context_parameters
+  elif (intent == 'PERMISSION_INTENT'):
+    intent_params['name'] = 'actions_intent_permission'
+    intent_params['parameters'] = {'PERMISSION': permission} 
+
+
+  contexts = [intent_params, \
+              {'name': 'requ', 'parameters': context_parameters}, \
+              {'name': 'actions_capability_screen_output', 'parameters': context_parameters}, \
+              {'name': 'actions_capability_audio_output', 'parameters': context_parameters}, \
+              {'name': 'google_assistant_input_type_keyboard', 'parameters': context_parameters}]
+
+  # TODO: Add other arguments as needed.
+  inputs = [{'intent': intent, 'raw_inputs': [{'query': raw_query}]}]
+  capabilities = [{'name': 'actions.capability.AUDIO_OUTPUT'}, {'name': 'actions.capability.SCREEN_OUTPUT'}]
+  original_request_data = {'inputs': inputs, 'isInSandbox': 'true', 'surface': {'capabilities': capabilities}, 'conversation': {'type': conversation_type}}
+  data['originalRequest'] = {'source': 'google', 'version':2, 'data': original_request_data}
+  metadata = {'intentName': intent}
+
+ 
+  data['result'] = {'parameters': parameters, 'contexts': contexts, 'resolved_query': resolved_query, 'source': 'agent', 'score': 1, 'metadata': metadata}
+  json_data = json.dumps(data)
+  print 'json data ', json_data
--- salah_com_fetcher_test.py
+++ salah_com_fetcher_test.py
@@ -5,4 +5,12 @@ def test_fetcher():
   """Tests that the fetcher gets data from salah.com"""
   print salah_com_fetcher.GetDailyPrayerTimes(37.3541079, -121.9552355)
 
+def test_JSON_builder():
+  """Tests that the JSON is built with the given inputs."""
+  print '\n when is maghrib', salah_com_fetcher.BuildJSON('maghrib', 'when is maghrib', 'when is maghrib', 'WHEN_IS_START_TIME_INTENT')
+  print '\n when is isha in fremont', salah_com_fetcher.BuildJSON('isha', 'when is isha in fremont', 'when is isha in fremont', 'WHEN_IS_START_TIME_INTENT', geo_city='fremont')
+  print '\n yes for permission', salah_com_fetcher.BuildJSON('maghrib', 'yes', 'actions_intent_PERMISSION', 'PERMISSION_INTENT', conversation_type='ACTIVE', permission='true')
+  print '\n no for permission', salah_com_fetcher.BuildJSON('fajr', 'no', 'actions_intent_PERMISSION', 'PERMISSION_INTENT', conversation_type='ACTIVE', permission='false')
+
 test_fetcher()
+test_JSON_builder()

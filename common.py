de9de7fbe759414faa9d0cabfef4337abc76e19d
Hashim Mir <hashim.mir123@gmail.com>
2017-06-03 05:33:13 -0700

    Lint fixes.

--- common.py
+++ common.py
@@ -1,18 +1,28 @@
+"""Common data objects referenced across the app."""
+
+# pylint: disable=too-few-public-methods
+
+
 class DailyPrayer(object):
-  UNSPECIFIED = 0;
-  FAJR = 1;
-  DHUHR = 2;
-  ASR = 3;
-  MAGHRIB = 4;
-  ISHA = 5;
-  QIYAM = 6;
+  """Server-wide canonical representation for daily prayers."""
+  UNSPECIFIED = 0
+  FAJR = 1
+  DHUHR = 2
+  ASR = 3
+  MAGHRIB = 4
+  ISHA = 5
+  QIYAM = 6
+
 
 class Intent(object):
+  """Server-wide canonical representation for Intents from API.AI."""
   UNSPECIFIED = 0
   WHEN_IS_PRAYER = 1
   WHEN_IS_PRAYER_NAME = 2
 
+
 class Locality(object):
+  """Locality implies the type of query - whether city-based or masjid-based."""
   UNSPECIFIED = 0
   CITY = 1
   MASJID = 2

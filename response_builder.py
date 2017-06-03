56015778b06986c8153312fd697f332221703e0d
Hashim Mir <hashim.mir123@gmail.com>
2017-06-03 06:14:05 -0700

    fixes pylint

--- response_builder.py
+++ response_builder.py
@@ -1,18 +1,22 @@
 """Helps build reponses in a format API.AI understands."""
 
+
 def RequestLocationPermission():
+  """Asks the usee permission to use their location."""
   return {
-    "data": {
-      "google":{
-        "expectUserResponse": 1,
-        "systemIntent": {
-          "intent":"actions.intent.PERMISSION",
-          "data":{
-            "@type": "type.googleapis.com/google.actions.v2.PermissionValueSpec",
-            "optContext":"To get you accurate timings",
-            "permissions":["DEVICE_PRECISE_LOCATION"]
+      "data": {
+          "google": {
+              "expectUserResponse": 1,
+              "systemIntent": {
+                  "intent": "actions.intent.PERMISSION",
+                  "data": {
+                      "@type":
+                      "type.googleapis.com/google.actions.v2.PermissionValueSpec",
+                      "optContext":
+                      "To get you accurate timings",
+                      "permissions": ["DEVICE_PRECISE_LOCATION"]
+                  }
+              }
           }
-        }
       }
-    }
   }

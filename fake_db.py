c6d1e300fe59ab356c1e86562a0299834faff5f2
Hashim Mir <hashim.mir123@gmail.com>
2017-06-03 05:51:39 -0700

    fixes fake_db pylint

--- fake_db.py
+++ fake_db.py
@@ -1,6 +1,6 @@
-"""This is a shim interface for a database until we have a real one set up."""
+"""This is a shim interface for a database until we have a real one set up.
 
-"""
+  Example database entry:
   "MmtdIf77tCKMRPdYEteAtv8EAWAj830zF5kQY9CB7HE=":
     {
       "city": "Mountain View",
@@ -9,17 +9,21 @@
     }
 """
 
-class FakeDb(object):
 
-  USER_TABLE_ = {
-  }
+class FakeDb(object):
+  """In-memory store of user ID to user info."""
+
+  USER_TABLE_ = {}
 
   def GetUserInfo(self, user_id):
+    """Gets info for a user."""
     return self.USER_TABLE_.get(user_id)
 
   def AddOrUpdateUser(self, user_id, user_info):
+    """Adds new user (or updates existing one) in the database."""
     self.USER_TABLE_[user_id] = user_info
 
   def DeleteUser(self, user_id):
+    """Deletes a user from the database."""
     if user_id in self.USER_TABLE_:
       del self.USER_TABLE_[user_id]

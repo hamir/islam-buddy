7c61bbf89b2bc8ce5dbd24282d1c38ad24cafeef
Hashim Mir <hashim.mir123@gmail.com>
2017-06-03 15:29:11 -0700

    final fix for linter

--- salah_com_fetcher_test.py
+++ salah_com_fetcher_test.py
@@ -1,8 +1,11 @@
-import salah_com_fetcher
-import requests
+"""Tests for salah_com_fetcher."""
 
-def test_fetcher():
+import salah_com_fetcher
+
+
+def TestFetcher():
   """Tests that the fetcher gets data from salah.com"""
   print salah_com_fetcher.GetDailyPrayerTimes(37.3541079, -121.9552355)
 
-test_fetcher()
+
+TestFetcher()
